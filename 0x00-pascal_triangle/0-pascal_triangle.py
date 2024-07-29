6d3d569e50dc5c345908b1dd8a0b691070bf5df2
Alaa Okair <alaaokair330@gmail.com>
2024-07-28 19:55:02 -0700

    task done

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"a module to Create a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n"
+
+
+def pascal_triangle(n):
+
