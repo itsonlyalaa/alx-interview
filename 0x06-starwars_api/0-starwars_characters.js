2f3bafbadb68748414ed4d1845b2f29db4c16f11
Alaa Okair <alaaokair330@gmail.com>
2024-09-05 18:53:46 -0700

    semistandard fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,21 +2,19 @@
 
 const request = require('request');
 if (process.argv.length === 3) {
-  const My_Args = process.argv.slice(2);
-  const Star_url = 'https://swapi-api.alx-tools.com/api/films/' + My_Args[0];
+  const myArg = process.argv.slice(2);
+  const starUrl = 'https://swapi-api.alx-tools.com/api/films/' + myArg[0];
   const options = { json: true };
 
-  request(Star_url, options, async function (error, res, body) {
+  request(starUrl, options, async function (error, res, body) {
     if (error) {
       console.log(error);
-    }
-    else {
+    } else {
       for (const char of body.characters) {
         const rt = () => {
           return new Promise((resolve, reject) => {
             request(char, options, function (error, res, body) {
-              if (error) { console.log(error); }
-              else {
+              if (error) { console.log(error); } else {
                 resolve(body.name);
               }
             });
